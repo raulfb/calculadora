95c7dae7c849785c5816d4f58ce3f6625da38303
Raul Franco <raul.franco@norvoz.es>
2022-11-08 11:07:06 +0100

    control de errores

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,34 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
     
-    let primero:String=args.nth(1).unwrap();
-    let operador:char=args.nth(0).unwrap().chars().next().unwrap();
-    let segundo:String=args.nth(0).unwrap();
+    let primero;
+    match args.nth(1) {
+        Some(v)=>{
+            primero=v;
+        },
+        None=>{
+            panic!("Error de sintaxis en num1. La sintaxis adecuada es: cargo run num1 operador num2");
+        }
+    }
+    let operador:char;
+    match args.nth(0) {
+        Some(v)=>{
+            operador=v.chars().next().unwrap();
+        },
+        None=>{
+            panic!("Error de sintaxis en operador. La sintaxis adecuada es: cargo run num1 operador num2");
+        }
+    }
+
+    let segundo;
+    match args.nth(0) {
+        Some(v)=>{
+            segundo=v;
+        },
+        None=>{
+            panic!("Error de sintaxis en num2. La sintaxis adecuada es: cargo run num1 operador num2");
+        }
+    }
 
     let primer_numero = primero.parse::<f32>().unwrap();
     let segundo_numero = segundo.parse::<f32>().unwrap();
